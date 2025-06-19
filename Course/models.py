4312ae9d79b0ef64bef9dd33667fb6801e92ca1e
ahmed-elsenussi <nagmstar91@gmail.com>
2025-06-19 11:53:38 +0300

    alter days to list, alter instructor to fk, alter, add relation of the classroom

--- Course/models.py
+++ Course/models.py
@@ -1,18 +1,48 @@
 from django.db import models
+from classroom.models import Classroom
+
+DAY = [
+    ('Sat', 'Saturday'),
+    ('Sun', 'Sunday'),
+    ('Mon', 'Monday'),
+    ('Tue', 'Tuesday'),
+    ('Wed', 'Wednesday'),
+    ('Thu', 'Thursday'),
+    ('Fri', 'Friday'),
+]
+
+
 
 from Level.models import Level
 class Course(models.Model):
+
     name = models.CharField(max_length=100)
     level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name='courses')
+
+
+    # LECTURE: DAY - TIME 
+    day_of_lecture = models.CharField(max_length=3, choices=DAY) # [SENU]: ALTERED TO CHOICES
     start_lecture = models.TimeField()
     end_lecture = models.TimeField()
+
+    # SECTION: DAY - TIME 
+    day_of_section = models.CharField(max_length=3, choices=DAY) # [SENU]: ALTERED TO CHOICES
     start_section = models.TimeField()
     end_section = models.TimeField()
-    day_of_lecture = models.CharField(max_length=20)
-    day_of_section = models.CharField(max_length=20)
-    instructor_lecture = models.CharField(max_length=100)
-    instructor_section = models.CharField(max_length=100)
+
+    # [SENU]l ATLER INSTRUCTOR
+    instructor_lecture = models.ForeignKey('Student.Instructor',on_delete=models.SET_NULL,null=True,blank=True,related_name='lecture_courses'  )
+    instructor_section = models.ForeignKey('Student.Instructor',on_delete=models.SET_NULL,null=True,blank=True,related_name='section_courses'  )
+
+
+
+
+    # [??]
     notes = models.TextField(blank=True)
 
-    def __str__(self):
-        return f"{self.name} ({self.level.name})"
+    # [SENU]: ADD CLASS (ONE-MANY)
+    # ----------------------------
+    classroom = models.ForeignKey(
+        Classroom, on_delete=models.CASCADE,
+        related_name='courses'  # [SENU] allows classroom.courses.all()
+    )
